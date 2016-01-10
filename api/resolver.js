c3426b8fc932f223d8af812707d8a3247b3ba5a6
Jamal Mohammed <jamal.mohammed@commonfloor.com>
2016-01-10 14:16:55 +0530

    Using qod api

--- api/resolver.js
+++ api/resolver.js
@@ -6,69 +6,26 @@ var _ = require('underscore');
 
 // The API that returns the in-email representation.
 module.exports = function(req, res) {
-  var term = req.query.text.trim();
-
-  if (/^http:\/\/giphy\.com\/\S+/.test(term)) {
-    // Special-case: handle strings in the special URL form that are suggested by the commandHint
-    // API. This is how the command hint menu suggests an exact Giphy image.
-    handleIdString(term.replace(/^http:\/\/giphy\.com\//, ''), req, res);
-  } else {
-    // Else, assume it was a search string.
-    handleSearchString(term, req, res);
+  var response;
+  try {
+    response = sync.await(request({
+      url: 'http://api.theysaidso.com/qod.json',
+      gzip: true,
+      json: true,
+      timeout: 15 * 1000
+    }, sync.defer()));
+  } catch (e) {
+    res.status(500).send('Error');
+    return;
   }
+
+  //var data = response.body.data;
+  var data = response.body.contents.quotes[0];
+  var quotation = data.quote;
+  var author = data.author;
+
+  var html = '<p>Quote of the day:<br>' + '<i>' + quotation + '</i><br> - ' + author + '</p>'
+  res.json({
+    body: html
+  });
 };
-
-function handleIdString(id, req, res) {
-  var response;
-  try {
-    response = sync.await(request({
-      url: 'http://api.giphy.com/v1/gifs/' + encodeURIComponent(id),
-      qs: {
-        api_key: key
-      },
-      gzip: true,
-      json: true,
-      timeout: 15 * 1000
-    }, sync.defer()));
-  } catch (e) {
-    res.status(500).send('Error');
-    return;
-  }
-
-  var image = response.body.data.images.original;
-  var width = image.width > 600 ? 600 : image.width;
-  var html = '<img style="max-width:100%;" src="' + image.url + '" width="' + width + '"/>';
-  res.json({
-    body: html
-    // Add raw:true if you're returning content that you want the user to be able to edit
-  });
-}
-
-function handleSearchString(term, req, res) {
-  var response;
-  try {
-    response = sync.await(request({
-      url: 'http://api.giphy.com/v1/gifs/random',
-      qs: {
-        tag: term,
-        api_key: key
-      },
-      gzip: true,
-      json: true,
-      timeout: 15 * 1000
-    }, sync.defer()));
-  } catch (e) {
-    res.status(500).send('Error');
-    return;
-  }
-
-  var data = response.body.data;
-
-  // Cap at 600px wide
-  var width = data.image_width > 600 ? 600 : data.image_width;
-  var html = '<img style="max-width:100%;" src="' + data.image_url + '" width="' + width + '"/>';
-  res.json({
-    body: html
-    // Add raw:true if you're returning content that you want the user to be able to edit
-  });
-}
